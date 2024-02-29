87d5d511a4a952f37f5c436ffa7e1056d2e9aae5
Vladimir Rybant <vrybant@gmail.com>
2024-02-29 16:34:04 +0300

    widget

---
+++ biblewidget/biblewidget.swift
@@ -0,0 +1,82 @@
+//
+//  biblewidget.swift
+//  biblewidget
+//
+//  Created by Vladimir Rybant 
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    }
+
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
+        SimpleEntry(date: Date(), configuration: configuration)
+    }
+    
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            entries.append(entry)
+        }
+
+        return Timeline(entries: entries, policy: .atEnd)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let configuration: ConfigurationAppIntent
+}
+
+struct biblewidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text("Time:")
+        Text(entry.date, style: .time)
+
+        Text("Favorite Emoji:")
+        Text(entry.configuration.favoriteEmoji)
+    }
+}
+
+struct biblewidget: Widget {
+    let kind: String = "biblewidget"
+
+    var body: some WidgetConfiguration {
+        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+            biblewidgetEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
+        }
+    }
+}
+
+extension ConfigurationAppIntent {
+    fileprivate static var smiley: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "😀"
+        return intent
+    }
+    
+    fileprivate static var starEyes: ConfigurationAppIntent {
+        let intent = ConfigurationAppIntent()
+        intent.favoriteEmoji = "🤩"
+        return intent
+    }
+}
+
+#Preview(as: .systemSmall) {
+    biblewidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
---
+++ biblewidget/biblewidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  biblewidgetBundle.swift
+//  biblewidget
+//
+//  Created by Vladimir Rybant 
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct biblewidgetBundle: WidgetBundle {
+    var body: some Widget {
+        biblewidget()
+    }
+}
