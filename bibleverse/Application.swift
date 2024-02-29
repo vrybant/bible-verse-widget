ddd959771398ba564136c905e2c853a65fa4f1d4
Vladimir Rybant <vrybant@gmail.com>
2024-02-29 16:28:17 +0300

    upload

---
+++ bibleverse/Application.swift
@@ -0,0 +1,17 @@
+//
+//  bibleverseApp.swift
+//  bibleverse
+//
+//  Created by Vladimir Rybant on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Application: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
